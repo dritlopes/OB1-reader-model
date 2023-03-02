fixed bug in computation of visual salience for where to move the eyes next

src/reading_functions.py
@@ -14,40 +14,9 @@ def get_stimulus_edge_positions(stimulus):
 
     return stimulus_word_edge_positions
 
-# def get_ngram_edge_position_weight(ngram, ngramLocations, stimulus_space_locations):
-#
-#     ngramEdgePositionWeight = 0.5  # This is just a default weight; in many cases, it's changed
-#     # to 1 or 2, as can be seen below.
-#     max_weight = 2.
-#
-#     if len(ngram) == 2:
-#         first = ngramLocations[0]
-#         second = ngramLocations[1]
-#
-#         if (first-1) in stimulus_space_locations and (second+1) in stimulus_space_locations:
-#             ngramEdgePositionWeight = max_weight
-#         elif (first+1) in stimulus_space_locations and (second-1) in stimulus_space_locations:
-#             ngramEdgePositionWeight = max_weight
-#         elif (first-1) in stimulus_space_locations or (second+1) in stimulus_space_locations:
-#             ngramEdgePositionWeight = 1.
-#         elif (first+1) in stimulus_space_locations or (second-1) in stimulus_space_locations:
-#             ngramEdgePositionWeight = 1.
-#     else:
-#         letter_location = ngramLocations
-#         # One letter word
-#         if letter_location-1 in stimulus_space_locations and letter_location+1 in stimulus_space_locations:
-#             ngramEdgePositionWeight = max_weight
-#         # letter at the edge
-#         elif letter_location-1 in stimulus_space_locations or letter_location+1 in stimulus_space_locations:
-#             ngramEdgePositionWeight = 1.
-#
-#     return ngramEdgePositionWeight
-
 def string_to_open_ngrams(string,gap):
 
-    all_ngrams = []
-    all_weights = []
-    all_locations = []
+    all_ngrams, all_weights, all_locations = [], [], []
     weight = 0.5
 
     # AL: make sure string contains one space before and after for correctly finding word edges
@@ -61,127 +30,25 @@ def string_to_open_ngrams(string,gap):
             # AL: letter at word edge receives higher weight
             if position in edge_locations:
                 weight = 1.0
-                # AL: include monogram if at word edge AL to MM: this was how it was done before (only monograms at the edge were added), is that the "bedoeling"? Or all monograms of a word should be added?
+                # AL: include monogram if at word edge
                 all_ngrams.append(letter)
                 all_weights.append(weight)
                 all_locations.append([position])
 
             # AL: find bigrams
             for i in range(1,gap+1):
+                # AL: make sure second letter of bigram does not cross the stimulus string nor the word
                 if position+i >= len(string) or string[position+i] == ' ':
                     break
                 bigram = letter+string[position+i]
                 all_ngrams.append(bigram)
-                all_weights.append(weight)
                 all_locations.append([position,position+i])
+                # AL: if both letters in bigram are edge letters, boost excitation
+                # if weight == 1.0 and position+i in edge_locations:
+                #     weight = 2.0
+                all_weights.append(weight)
 
     return all_ngrams, all_weights, all_locations
-
-# def string_to_ngrams_and_locations(stimulus,pm, is_suffix=False):
-#
-#     stimulus_space_positions = get_stimulus_space_positions(stimulus)
-#     stimulus = "_" + stimulus + "_"  # GS try to recognize small words better, does not work doing this
-#     # For the current stimulus, bigrams will be made. Bigrams are only made
-#     # for letters that are within a range of 4 from each other; (gap=3)
-#
-#     # Bigrams that contain word boundary letters have more weight.
-#     # This is done by means of locating spaces in stimulus, and marking
-#     # letters around space locations (as well as spaces themselves), as
-#     # indicators of more bigram weight.
-#
-#     """Returns list with all unique open bigrams that can be made of the stim, and their respective locations
-#     (called 'word' for historic reasons), restricted by the maximum gap between two intervening letters."""
-#     all_bigrams = []
-#     bigrams_to_locations = {}
-#     gap = pm.bigram_gap  # None = no limit
-#     if gap == None:
-#         for first in range(len(stimulus) - 1):
-#             if (stimulus[first] == " "):
-#                 continue
-#             for second in range(first + 1, len(stimulus)):
-#                 if (stimulus[second] == " "):
-#                     break
-#                 bigram = stimulus[first] + stimulus[second]
-#                 if bigram != '  ':
-#                     if not bigram in all_bigrams:
-#                         all_bigrams.append(bigram)
-#                     bigram_edge_position_weight = get_ngram_edge_position_weight(
-#                         bigram, (first, second), stimulus_space_positions)
-#                     if (bigram in bigrams_to_locations.keys()):
-#                         bigrams_to_locations[bigram].append((first, second, bigram_edge_position_weight))
-#                     else:
-#                         bigrams_to_locations[bigram] = [(first, second, bigram_edge_position_weight)]
-#     else:
-#         for first in range(len(stimulus) - 1):
-#             # NV: this code implant is meant to insert special suffix bigrams in bigram list
-#             # TODO: does not work for prefixes yet
-#             if (stimulus[first] == " "):  # NV: means that first letter is index 1 or last+1
-#                 if first == 1:  # NV: if it is in the beginning of word
-#                     if is_suffix:
-#                         continue  # dont do the special _ at beginning of word if is affix
-#                     second_alt = 2  # NV: _alt not to interfere with loop variables
-#                     bigram = '_' + stimulus[second_alt]
-#                     if not bigram in all_bigrams:
-#                         all_bigrams.append(bigram)
-#                     bigram_edge_position_weight = get_ngram_edge_position_weight(
-#                         bigram, (first, second_alt), stimulus_space_positions)
-#                     if (bigram in bigrams_to_locations.keys()):
-#                         bigrams_to_locations[bigram].append((first, second_alt, bigram_edge_position_weight))
-#                     else:
-#                         bigrams_to_locations[bigram] = [(first, second_alt, bigram_edge_position_weight)]
-#                     continue
-#                 elif first == len(stimulus) - 2:  # NV: if first letter is the end space
-#                     first_alt = -3  # NV: index of last letter
-#                     # NV: get the actual index (you do +, because first alt is a negative number)
-#                     first_alt = len(stimulus) + first_alt
-#                     second_alt = -2  # NV: index of space after last letter
-#                     second_alt = len(stimulus) + second_alt
-#                     bigram = stimulus[first_alt] + '_'
-#                     if not bigram in all_bigrams:
-#                         all_bigrams.append(bigram)
-#                     bigram_edge_position_weight = get_ngram_edge_position_weight(
-#                         bigram, (first_alt, second_alt), stimulus_space_positions)
-#                     if (bigram in bigrams_to_locations.keys()):
-#                         bigrams_to_locations[bigram].append((first_alt, second_alt, bigram_edge_position_weight))
-#                     else:
-#                         bigrams_to_locations[bigram] = [(first_alt, second_alt, bigram_edge_position_weight)]
-#                     continue
-#
-#             # NV:pick letter between first+1 and first+1+gap+1 (end of bigram max length), as long as that is smaller than end of word
-#             for second in range(first + 1, min(first + 1 + gap + 1, len(stimulus))):
-#                 if (stimulus[
-#                     second] == " "):  # NV: if that is second lettter, you know you have reached the end of possible bigrams
-#                     # NV: break out of second loop if second stim is __. This means symbols before word, or when end of word is reached.
-#                     break
-#                 bigram = stimulus[first] + stimulus[second]
-#                 if bigram != '  ':
-#                     if not bigram in all_bigrams:
-#                         all_bigrams.append(bigram)
-#                     bigram_edge_position_weight = get_ngram_edge_position_weight(
-#                         bigram, (first, second), stimulus_space_positions)
-#                     if (bigram in bigrams_to_locations.keys()):
-#                         bigrams_to_locations[bigram].append((first, second, bigram_edge_position_weight))
-#                     else:
-#                         bigrams_to_locations[bigram] = [(first, second, bigram_edge_position_weight)]
-#
-#     # # GS: test of dit werkt, dan geen monograms, die worden later toch weggefilterd
-#     #    """Also add monograms"""
-#     #    for position in range(len(stimulus)):
-#     #        monogram=stimulus[position]
-#     #        if(monogram==" "):
-#     #            continue
-#     #
-#     #        if not monogram in allBigrams:
-#     #            allBigrams_append(monogram)
-#     #
-#     #        monogramEdgePositionWeight = get_ngram_edge_position_weight(monogram, position,stimulus_space_positions)
-#     #        if monogram in bigramsToLocations.keys():
-#     #            bigramsToLocations[monogram].append((position,monogramEdgePositionWeight))
-#     #        else:
-#     #            bigramsToLocations[monogram]=[(position,monogramEdgePositionWeight)]
-#     # # edge_bigrams = ["_"+stimulus[2], stimulus[-3]+"_"]  # NV: added first and last bigram for affix recognition.
-#
-#     return all_bigrams, bigrams_to_locations
 
 def normalize_values(p, values, max_value):
 
@@ -358,7 +225,11 @@ def calc_monogram_attention_sum(position_start, position_end, eye_position, atte
     # this is only used to calculate where to move next when forward saccade
     sum_attention_letters = 0
 
+    # AL: make sure letters to the left of fixated word are not included
+    if foveal_word: position_start = eye_position + 1
+
     for letter_location in range(position_start, position_end+1):
+        # print(letter_location)
         monogram_locations_weight_multiplier = 0.5
         if foveal_word:
             if letter_location == position_end:
@@ -369,10 +240,10 @@ def calc_monogram_attention_sum(position_start, position_end, eye_position, atte
         # Monogram activity depends on distance of monogram letters to the centre of attention and fixation
         attention_eccentricity = letter_location - attention_position
         eye_eccentricity = abs(letter_location - eye_position)
-
+        # print(attention_eccentricity, eye_eccentricity)
         attention = get_attention_skewed(attend_width, attention_eccentricity, attention_skew)
         visual_acuity = calc_acuity(eye_eccentricity, let_per_deg)
-
+        # print(attention,visual_acuity)
         sum_attention_letters += (attention * visual_acuity) * monogram_locations_weight_multiplier
 
     return sum_attention_letters
@@ -387,6 +258,7 @@ def calc_word_attention_right(word_edges, eye_position, attention_position, atte
 
         # if n or n + x (but not n - x), so only fixated word or words to the right
         if i >= fixated_position_in_stimulus:
+            # print(i,edges)
             word_start_edge = edges[0]
             word_end_edge = edges[1]
 
@@ -400,7 +272,7 @@ def calc_word_attention_right(word_edges, eye_position, attention_position, atte
                 crt_word_monogram_attention_sum = 0
             else:
                 crt_word_monogram_attention_sum = calc_monogram_attention_sum(word_start_edge, word_end_edge, eye_position, attention_position, attend_width, attention_skew, let_per_deg, foveal_word)
-
+            # print('word position and visual salience: ',i,crt_word_monogram_attention_sum)
             word_attention_right.append(crt_word_monogram_attention_sum)
 
     return word_attention_right
@@ -415,3 +287,14 @@ def calc_saccade_error(saccade_distance, optimal_distance, saccErr_scaler, saccE
         return saccade_error_norm
     else:
         return 0.
+
+def check_previous_refixations_at_position(all_data, fixation, fixation_counter, max_n_refix):
+
+    previous_refixations = np.zeros(max_n_refix+1,dtype=bool)
+
+    for i in range(max_n_refix+1):
+        if fixation_counter - i in all_data.keys():
+            if all_data[fixation_counter - i]['saccade_type'] == 'refixation' and all_data[fixation_counter - i]['foveal word index'] == fixation:
+                previous_refixations[i] = True
+
+    return previous_refixations

src/simulate_experiment.py
@@ -5,7 +5,8 @@ from collections import defaultdict
 
 from utils import get_word_freq, get_pred_values, check_previous_inhibition_matrix
 from reading_functions import get_threshold, string_to_open_ngrams, build_word_inhibition_matrix, cal_ngram_exc_input, define_slot_matching_order, is_similar_word_length, \
-    sample_from_norm_distribution, find_word_edges, get_midword_position_for_surrounding_word, calc_word_attention_right, update_threshold, calc_saccade_error
+    sample_from_norm_distribution, find_word_edges, get_midword_position_for_surrounding_word, calc_word_attention_right, update_threshold, calc_saccade_error,\
+    check_previous_refixations_at_position
 
 
 logger = logging.getLogger(__name__)
@@ -43,7 +44,7 @@ def compute_ngram_activity(stimulus,eye_position,attention_position,attend_width
         else:
             unit_activations[ngram] = activation
 
-    print(unit_activations)
+    # print(unit_activations)
     return unit_activations
 
 def compute_words_input(stimulus, lexicon_word_ngrams, eye_position, attention_position, attend_width, pm):
@@ -139,15 +140,7 @@ def match_active_words_to_input_slots(order_match_check, stimulus, recognized_po
 
     return recognized_position_flag, recognized_word_at_position, recognized_word_at_position_flag, lexicon_word_activity, new_recognized_words
 
-def compute_next_attention_position(all_data,tokens,fixation,word_edges,fixated_position_in_stimulus,regression_flag,recognized_position_flag,lexicon_word_activity,eye_position,fixation_counter,attention_position,attend_width,fix_lexicon_index,pm):
-
-    # AL: re-set saccade info from current fixation to update it with next fixation
-    saccade_info = {'saccade_type': None,
-                    'saccade_distance': 0,
-                    'saccade_error': 0,
-                    'saccade_cause': 0,
-                    'saccade_type_by_error': False,
-                    'offset_from_word_center': 0}
+def compute_next_attention_position(all_data,tokens,fixation,word_edges,fixated_position_in_stimulus,regression_flag,recognized_position_flag,lexicon_word_activity,eye_position,fixation_counter,attention_position,attend_width,fix_lexicon_index,pm,saccade_info):
 
     # Define target of next fixation relative to fixated word n (i.e. 0=next fix on word n, -1=fix on n-1, etc). Default is 1 (= to word n+1)
     next_fixation = 1
@@ -163,6 +156,9 @@ def compute_next_attention_position(all_data,tokens,fixation,word_edges,fixated_
 
     # refixation: refixate if the foveal word is not recognized but is still being processed
     elif (not recognized_position_flag[fixation]) and (lexicon_word_activity[fix_lexicon_index] > 0):
+        # # AL: only allows 5 consecutive refixations on the same word to avoid infinitely refixating if no word reaches threshold recognition at a given position
+        # previous_refix = check_previous_refixations_at_position(all_data, fixation, fixation_counter, max_n_refix=5)
+        # if False in previous_refix:
         word_reminder_length = word_edges[fixated_position_in_stimulus][1] - eye_position
         if word_reminder_length > 0:
             next_fixation = 0
@@ -211,7 +207,8 @@ def compute_next_attention_position(all_data,tokens,fixation,word_edges,fixated_
             saccade_info['saccade_cause'] = 2 # AL: bcs right of fixated word has highest attwght (letter excitation)
 
     # AL: saccade distance is next attention position minus the current eye position
-    saccade_info['saccade_distance'] = attention_position - eye_position
+    if attention_position:
+        saccade_info['saccade_distance'] = attention_position - eye_position
 
     return attention_position, saccade_info
 
@@ -367,7 +364,7 @@ def continuous_reading(pm,tokens,word_overlap_matrix,lexicon_word_ngrams,lexicon
 
     while not end_of_task:
 
-        print(f'---Fixation {fixation_counter+1} at position {fixation+1}---')
+        print(f'---Fixation {fixation_counter} at position {fixation}---')
 
         fixation_data = defaultdict(list)
 
@@ -392,8 +389,7 @@ def continuous_reading(pm,tokens,word_overlap_matrix,lexicon_word_ngrams,lexicon
         eye_position = compute_eye_position(stimulus, fixated_position_in_stimulus, saccade_info['offset_from_word_center'])
         fixation_data['stimulus'] = stimulus
         fixation_data['eye position'] = eye_position
-        print(
-            f"Stimulus: {stimulus}\neye position in stimulus: {eye_position}\nfour characters to the right of fixation: {stimulus[eye_position:eye_position + 4]}")
+        print(f"Stimulus: {stimulus}\neye position in stimulus: {eye_position}\nfour characters to the right of fixation: {stimulus[eye_position:eye_position + 4]}")
 
         # define order to match activated words to slots in the stimulus
         # NV: the order list should reset when stimulus changes or with the first stimulus
@@ -424,7 +420,7 @@ def continuous_reading(pm,tokens,word_overlap_matrix,lexicon_word_ngrams,lexicon
         n_ngrams, total_ngram_activity, all_ngrams, word_input = compute_words_input(stimulus, lexicon_word_ngrams, eye_position, attention_position, attend_width, pm)
         fixation_data['n_ngrams'] = n_ngrams
         fixation_data['total_ngram_activity'] = total_ngram_activity
-        print("input to fixwrd: " + str(round(word_input[tokens_to_lexicon_indices[fixation]], 3)))
+        print("input to fixwrd at first cycle: " + str(round(word_input[tokens_to_lexicon_indices[fixation]], 3)))
 
         # Counter n_cycles_since_attent_shift is 0 until attention shift (saccade program initiation), then starts counting to 5
         #   (because a saccade program takes 5 cycles, or 125ms.)
@@ -496,7 +492,13 @@ def continuous_reading(pm,tokens,word_overlap_matrix,lexicon_word_ngrams,lexicon
                 # shift attention (& plan saccade in 125 ms) if n_cycles is higher than random threshold shift_start
                 if n_cycles >= shift_start:
                     shift = True
-                    # offset_from_word_center = 0
+                    # AL: re-set saccade info from current fixation to update it with next fixation
+                    saccade_info = {'saccade_type': None,
+                                    'saccade_distance': 0,
+                                    'saccade_error': 0,
+                                    'saccade_cause': 0,
+                                    'saccade_type_by_error': False,
+                                    'offset_from_word_center': 0}
                     attention_position, saccade_info = compute_next_attention_position(all_data,
                                                                                         tokens,
                                                                                         fixation,
@@ -510,15 +512,19 @@ def continuous_reading(pm,tokens,word_overlap_matrix,lexicon_word_ngrams,lexicon
                                                                                         attention_position,
                                                                                         attend_width,
                                                                                         foveal_word_index,
-                                                                                        pm)
+                                                                                        pm,
+                                                                                        saccade_info)
                     fixation_data['foveal word activity at shift'] = fixation_data['foveal word activity per cycle'][-1]
                     print('attentpos ', attention_position)
-                    # recompute word input, using ngram excitation and inhibition, because attentshift changes bigram input
-                    n_ngrams, total_ngram_activity, all_ngrams, word_input = compute_words_input(stimulus,
-                                                                                                lexicon_word_ngrams,
-                                                                                                eye_position,
-                                                                                                attention_position,
-                                                                                                attend_width, pm)
+                    # AL: attention position is None if at the end of the text and saccade is not refixation nor regression, so do not compute new words input
+                    if attention_position:
+                        # AL: recompute word input, using ngram excitation and inhibition, because attentshift changes bigram input
+                        n_ngrams, total_ngram_activity, all_ngrams, word_input = compute_words_input(stimulus,
+                                                                                                    lexicon_word_ngrams,
+                                                                                                    eye_position,
+                                                                                                    attention_position,
+                                                                                                    attend_width, pm)
+                        attention_position = np.round(attention_position)
 
             if shift:
                 n_cycles_since_attent_shift += 1 # ...count cycles since attention shift
@@ -528,7 +534,6 @@ def continuous_reading(pm,tokens,word_overlap_matrix,lexicon_word_ngrams,lexicon
                 recognized_word_at_cycle[fixation] = n_cycles
                 fixation_data['recognition cycle'] = recognized_word_at_cycle[fixation]
 
-            attention_position = np.round(attention_position)
             n_cycles += 1
 
         # out of cycle loop. After last cycle, compute fixation duration and add final values for fixated word before shift is made
@@ -552,23 +557,23 @@ def continuous_reading(pm,tokens,word_overlap_matrix,lexicon_word_ngrams,lexicon
             print("No word was recognized at fixation position")
 
         fixation_counter += 1
+        print(fixation_data)
 
-        # Check if end of text is reached
-        if fixation == total_n_words - 1:
+        # Check if end of text is reached AL: if fixation on last word and next saccade not refixation nor regression
+        if fixation == total_n_words - 1 and saccade_info['saccade_type'] not in ['refixation', 'regression']:
             end_of_task = True
             print("END REACHED!")
             continue
 
-        print(fixation_data)
-        if fixation_counter > 10: exit()
+        #if fixation_counter > 10: exit()
         # if end of text is not yet reached, compute next eye position and thus next fixation
         fixation, next_eye_position, saccade_info = compute_next_eye_position(pm, saccade_info, eye_position, stimulus, fixation, total_n_words, word_edges, fixated_position_in_stimulus)
 
-        # stop if next fixation will be at the last word of the text and if next eye position is at (last letter -1) of text to prevent errors
-        if fixation == total_n_words - 1 and next_eye_position >= len(stimulus) - 3:
-            end_of_task = True
-            print("END REACHED!")
-            continue
+        # # stop if next fixation will be at the last word of the text and if next eye position is at (last letter -1) of text to prevent errors
+        # if fixation == total_n_words - 1 and next_eye_position >= len(stimulus) - 3:
+        #     end_of_task = True
+        #     print("END REACHED!")
+        #     continue
 
     # register words in text in which no word in lexicon reaches recognition threshold
     unrecognized_words = dict()