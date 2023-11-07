fixed formula for defining new eye position

src/analysis_predictability.py
@@ -19,7 +19,7 @@ def read_in_pred_files(pred_map_filepaths):
 
     for predictor, filepath in pred_map_filepaths.items():
         with open(filepath) as f:
-            pos_pred_maps[predictor]=json.load(f)
+            pos_pred_maps[predictor] = json.load(f)
 
     return pos_pred_maps
 
@@ -267,38 +267,42 @@ def plot_sim_results_pred(filepaths, measures):
 
         x, y, data_type, predictors = [], [], [], []
 
-        for predictor in ['cloze', 'llama']:
+        for predictor in ['cloze', 'gpt2', 'llama']:
+
+            model_values, human_values, pred_values = [], [], []
+
             for data_name, data in data_log.items():
-                if f'simulation_eye_movements_mean_Provo_Corpus_continuous_reading_{predictor}' in data_name:
+                if f'simulation_eye_movements_mean_Provo_corpus_continuous_reading_{predictor}' in data_name:
                     results_dir = os.path.dirname(data_name).replace('model_output', 'analysed')
                     pred_values = data['predictability'].tolist()
                     model_values = data[measure].tolist()
-                elif 'Provo_Corpus_eye_tracking_mean.csv' in data_name:
+                elif 'Provo_Corpus_eye_tracking_last_sim_mean.csv' in data_name:
                     human_values = data[measure].tolist()
 
-            for eye_movement in [model_values, human_values]:
-                # drop nan values
-                pred_values_clean, eye_movement_clean = drop_nan_values(np.array(pred_values), np.array(eye_movement))
-                # bin data
-                y_means, edges, _ = binned_statistic(pred_values_clean, eye_movement_clean, statistic='mean', bins=50)
-                x_bins = edges[:-1]
-                x.extend(x_bins)
-                y.extend(y_means)
-                if eye_movement == model_values:
-                    type = 'OB1-reader'
-                else:
-                    type = 'PROVO'
-                data_type.extend([type for bin in x_bins])
-                predictors.extend([predictor for bin in x_bins])
+            if len(model_values) > 0 and len(human_values) > 0 and len(pred_values) > 0:
+                for eye_movement in [model_values, human_values]:
+                    # drop nan values
+                    pred_values_clean, eye_movement_clean = drop_nan_values(np.array(pred_values), np.array(eye_movement))
+                    # bin data
+                    y_means, edges, _ = binned_statistic(pred_values_clean, eye_movement_clean, statistic='mean', bins=50)
+                    x_bins = edges[:-1]
+                    x.extend(x_bins)
+                    y.extend(y_means)
+                    if eye_movement == model_values:
+                        type = 'OB1-reader'
+                    else:
+                        type = 'PROVO'
+                    data_type.extend([type for bin in x_bins])
+                    predictors.extend([predictor for bin in x_bins])
 
-        # seaborn lmplot function requires dataframe
-        df = pd.DataFrame({'predictability': x, measure: y, 'predictor': data_type, 'condition': predictors})
-        plt.figure()
-        plot = sb.lmplot(data=df, x='predictability', y=measure, hue='predictor', col='condition')
-        results_dir = f'{results_dir}/plots'
-        if not os.path.isdir(results_dir): os.makedirs(results_dir)
-        plot.figure.savefig(f'{results_dir}/plot_{measure}.png')
-        plt.close()
+        if len(data_type) > 0:
+            # seaborn lmplot function requires dataframe
+            df = pd.DataFrame({'predictability': x, measure: y, 'predictor': data_type, 'condition': predictors})
+            plot = sb.lmplot(data=df, x='predictability', y=measure, hue='predictor', col='condition')
+            results_dir = f'{results_dir}/plots'
+            if not os.path.isdir(results_dir): os.makedirs(results_dir)
+            plot.figure.savefig(f'{results_dir}/plot_{measure}.png')
+            plt.close()
 
 def test_correlation(pred_values, eye_movement_values, filepath):
 
@@ -345,9 +349,10 @@ def main():
     unknown_map_filepaths = {'cloze': '../data/processed/prediction_map_Provo_Corpus_cloze_continuous_reading_english_unknown.json',
                              'GPT2': '../data/processed/prediction_map_Provo_Corpus_gpt2_continuous_reading_english_topkall_unknown.json',
                              'LLAMA': '../data/processed/prediction_map_Provo_Corpus_llama_continuous_reading_english_topkall_unknown.json'}
-    results_filepaths = ["../data/analysed/_19_10_2023_13-53-17/simulation_eye_movements_mean_Provo_Corpus_continuous_reading_cloze_0.1.csv",
-                         "../data/analysed/_19_10_2023_13-53-17/simulation_eye_movements_mean_Provo_Corpus_continuous_reading_llama_0.1.csv",
-                         "../data/processed/Provo_Corpus_eye_tracking_mean.csv"]
+    results_filepaths = ["../data/analysed/_06_11_2023_15-29-19/simulation_eye_movements_mean_Provo_corpus_continuous_reading_baseline_0.1.csv",
+                        "../data/analysed/_06_11_2023_15-29-19/simulation_eye_movements_mean_Provo_corpus_continuous_reading_cloze_0.1.csv",
+                        "../data/analysed/_06_11_2023_15-29-19/simulation_eye_movements_mean_Provo_corpus_continuous_reading_gpt2_0.1.csv",
+                         "../data/processed/Provo_Corpus_eye_tracking_last_sim_mean.csv"]
     eye_movement_filepath = '../data/processed/Provo_Corpus_eye_tracking_mean.csv'
     measures = ['skip',
                 'single_fix',
@@ -385,10 +390,10 @@ def main():
     # word_pred_acc(pred_maps)
     #
     # plot results on predictability
-    # plot_sim_results_pred(results_filepaths, measures)
+    plot_sim_results_pred(results_filepaths, measures)
 
     # test correlation between predictability and eye movements
-    test_correlation_pred(eye_movement_filepath, measures, pred_maps)
+    # test_correlation_pred(eye_movement_filepath, measures, pred_maps)
 
 if __name__ == '__main__':
     main()

src/evaluation.py
@@ -671,7 +671,7 @@ def compute_all_error(parameters, output_filepath, mean_true_eye_movements, mean
     filepath = output_filepath.replace('model_output', 'analysed').replace('simulation_', f'RM2E_mean_eye_movements_')
     filepath = create_new_directory(filepath, 'RM2E')
     mean2error_df.to_csv(filepath, sep='\t', index=False)
-
+    data_log[filepath] = mean2error_df
     if verbose:
         print(mean2error_df.head(len(parameters.evaluation_measures) + 1))
 
@@ -804,6 +804,19 @@ def plot_error(error_values, conditions, measure, filepath):
     plot.figure.savefig(filepath)
     plt.close()
 
+def plot_raw_measures(raw_values, conditions, measure, filepath):
+
+    plt.figure()
+    plot = sb.barplot(x=conditions, y=raw_values, errorbar="sd",
+                     errwidth=0.5) # TODO upate version seaborn to use err_kws argument
+    # err_kws={"alpha": 0.2, "linewidth": 0.5}
+    plot.set(ylabel=measure)
+    plot.bar_label(plot.containers[0])
+    dir = os.path.dirname(filepath)
+    filepath = f"{dir}/plot_{measure}_raw.png"
+    plot.figure.savefig(filepath)
+    plt.close()
+
 def test_difference(measure, sim_values1, sim_values2, filepath):
 
     # https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.ttest_rel.html
@@ -831,7 +844,6 @@ def test_difference(measure, sim_values1, sim_values2, filepath):
     # https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.wilcoxon.html
     # It is a non-parametric version of the paired T-test
     test = stats.wilcoxon(sim_values1, sim_values2)
-    print(test)
     t_test = {'test': ['t-statistics', 'p-value'],
               'result': [test.statistic, test.pvalue]}
     # Wilcoxon statistic: the sum of the ranks of the differences above zero
@@ -841,36 +853,38 @@ def test_difference(measure, sim_values1, sim_values2, filepath):
 def compare_conditions(parameters_list, data_log):
 
     if data_log:
-
         measures = parameters_list[0].evaluation_measures
 
         for measure in measures:
-
-            sim_values1, sim_values2, error_values, conditions = [], [], [], []
-            for condition in ['baseline', 'cloze', 'llama']:
+            sim_values1, sim_values2, error_values, raw_values, conditions_raw, conditions_error = [], [], [], [], [], []
+            for condition in ['Provo', 'baseline', 'cloze', 'gpt2']:
                 for data_name, data in data_log.items():
+                    if 'mean' in data_name and condition in data_name:
+                        if 'simulation' in data_name or 'last_sim' in data_name:
+                            raw_values.extend(data[measure].tolist())
+                            conditions_raw.extend([condition.lower() for value in data[measure].tolist()])
                     if 'RM2E' in data_name and condition in data_name:
                         if 'mean' in data_name:
                             measure_value = \
                             data.loc[data["eye_tracking_measure"] == measure]['norm_mean_squared_error'].tolist()[0]
                             error_values.append(float(measure_value))
-                            conditions.append(condition)
+                            conditions_error.append(condition)
                         elif condition == 'cloze':
                             sim_values1 = data[measure].tolist()
-                        elif condition == 'llama':
+                        elif condition == 'gpt2':
                             sim_values2 = data[measure].tolist()
 
             # ----------- Plot errors each condition per measure -----------
-            if len(error_values) > 0:
-                filepath = parameters_list[0].results_filepath.replace('model_output', 'analysed').replace('cloze',
-                                                                                                           '').replace(
-                    'llama', '')
-                filepath = create_new_directory(filepath, 'plots')
-                plot_error(error_values, conditions, measure, filepath)
+            filepath = parameters_list[0].results_filepath.replace('model_output', 'analysed').replace('cloze','').replace('gpt2', '')
+            filepath = create_new_directory(filepath, 'plots')
+            # if len(error_values) > 0:
+            #     plot_error(error_values, conditions_error, measure, filepath)
+            if len(raw_values) > 0:
+                plot_raw_measures(raw_values, conditions_raw, measure, filepath)
 
             # ----------- Test significance of difference in mean2error between conditions -----------
             if len(sim_values1) > 0 and len(sim_values2) > 0:
-                filepath = parameters_list[0].results_filepath.replace('cloze', '').replace('llama', '')
+                filepath = parameters_list[0].results_filepath.replace('cloze', '').replace('gpt2', '')
                 filepath = filepath.replace('__', f'_{measure}_t-test_').replace('model_output', 'analysed')
                 filepath = create_new_directory(filepath, 't-test')
                 test_difference(measure, sim_values1, sim_values2, filepath)
@@ -918,6 +932,13 @@ def evaluate_output (parameters_list: list, verbose=True):
             # ----------- Word recognition accuracy -----------
             compute_word_recog_acc(simulation_output_all, simulation_output, parameters, output_filepath, verbose)
 
+    # paths = ["../data/processed/Provo_Corpus_eye_tracking_last_sim_mean.csv",
+    #          "../data/analysed/_03_11_2023_11-44-35/simulation_eye_movements_mean_Provo_corpus_continuous_reading_baseline_0.1.csv",
+    #          "../data/analysed/_03_11_2023_11-44-35/simulation_eye_movements_mean_Provo_corpus_continuous_reading_cloze_0.1.csv",
+    #          "../data/analysed/_03_11_2023_11-44-35/simulation_eye_movements_mean_Provo_corpus_continuous_reading_gpt2_0.1.csv"]
+    # for path in paths:
+    #     df = pd.read_csv(path, sep='\t')
+    #     data_log[path] = df
     compare_conditions(parameters_list, data_log)
 
 

src/reading_components.py
@@ -375,12 +375,12 @@ def compute_next_attention_position(all_data,tokens,fixation,word_edges,fixated_
         next_fixation = -1
 
     # skip bcs regression: if the current fixation was a regression
-    # elif regression_flag[fixation]:
-    #     # go to the nearest non-recognized word to the right within stimulus
-    #     for i in [1, 2]:
-    #         if fixation + i < len(tokens):
-    #             if recognized_word_at_position[fixation + i]:
-    #                 next_fixation = i + 1
+    elif regression_flag[fixation]:
+        # go to the nearest non-recognized word to the right within stimulus
+        for i in [1, 2]:
+            if fixation + i < len(tokens):
+                if recognized_word_at_position[fixation + i]:
+                    next_fixation = i + 1
 
     # refixation: refixate if the foveal word is not recognized but is still being processed
     elif (not recognized_word_at_position[fixation]) and (lexicon_word_activity[fix_lexicon_index] > 0):
@@ -465,11 +465,11 @@ def compute_next_eye_position(pm, attention_position, eye_position, fixation, fi
     saccade_info['saccade_error'] = float(saccade_error)
 
     # compute the position of next fixation
-    eye_position = int(np.round(eye_position + saccade_distance))
-    # if saccade_distance < 0:
-    #     eye_position = int(math.floor(eye_position + saccade_distance))
-    # else:
-    #     eye_position = int(math.ceil(eye_position + saccade_distance))
+    # eye_position = int(np.round(eye_position + saccade_distance))
+    if saccade_distance < 0:
+        eye_position = int(math.floor(eye_position + saccade_distance))
+    else:
+        eye_position = int(math.ceil(eye_position + saccade_distance))
     if verbose:
         print(f'next eye position: {eye_position}')
     logger.info(f'next eye position: {eye_position}')